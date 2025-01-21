5113df3d1e3487402244d21d05eeb798125d92a7
vitalie stinca <vitalicstinca123@gmail.com>
2025-01-21 17:29:21 +0000

    feat: Experiment with path operations

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ app = FastAPI()
 
 @app.get('/')
 async def root():
-    return {"message": "Hello world"}
+    return {"message": "Welcome to my API"}
+
+@app.get('/posts')
+def get_posts():
+    return {"data": "This is your posts"}
