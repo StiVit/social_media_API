95a08373b2b785a7d6a1da018114b813acc3008b
vitalie stinca <vitalicstinca123@gmail.com>
2025-01-28 23:42:07 +0000

    feat: Create the model for the posts

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from .database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    published = Column(Boolean, default=True)
